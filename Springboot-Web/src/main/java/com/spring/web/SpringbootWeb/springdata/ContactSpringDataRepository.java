ebb4bd6d09d5acca200b56ab7f2a7e14d4c39a3d
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-09-23 23:48:20 +0530

    include adding method to web application

--- Springboot-Web/src/main/java/com/spring/web/SpringbootWeb/springdata/ContactSpringDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.web.SpringbootWeb.springdata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.web.SpringbootWeb.entity.Contact;
-
-public interface ContactSpringDataRepository extends JpaRepository<Contact, Integer>{
-
-}
